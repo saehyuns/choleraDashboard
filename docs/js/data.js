0dfb23acb430a6d10b964030a02f548bf8ccfce3
Sae Hyun Song <17700563+saehyuns@users.noreply.github.com>
2018-09-19 13:24:26 -1000

    HELLO

---
+++ docs/js/data.js
@@ -0,0 +1,5 @@
+//     var choleraDeathLocations = [];
+//     var choleraPumpLocations = [];
+//     var choleraDeaths = [];
+//     var naplesCholeraAgeSexData = [];
+//     var UKcensus1851 = [];
